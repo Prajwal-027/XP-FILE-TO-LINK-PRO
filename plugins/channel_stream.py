a3dbb6ca9f8d3c9135a9a105108a79f5631436fd
Prajwal-027 <gamerprajwalxp@gmail.com>
2026-01-21 09:29:35 +0530

    Update channel_stream.py
    Signed-off-by: Prajwal-027 <gamerprajwalxp@gmail.com>

--- plugins/channel_stream.py
+++ plugins/channel_stream.py
@@ -22,7 +22,7 @@ async def channel_receive_handler(bot: Client, broadcast: Message):
                     await bot.send_message(
                         chat_id,
                         f"🚫 **Tʜɪꜱ ᴄʜᴀɴɴᴇʟ ɪꜱ ʙᴀɴɴᴇᴅ ғʀᴏᴍ ᴜꜱɪɴɢ ᴛʜᴇ ʙᴏᴛ.**\n\n"
-                        f"🔄 **Cᴏɴᴛᴀᴄᴛ ᴀᴅᴍɪɴ ɪғ ʏᴏᴜ ᴛʜɪɴᴋ ᴛʜɪꜱ ɪꜱ ᴀ ᴍɪꜱᴛᴀᴋᴇ.**\n\n@AV_OWNER_BOT"
+                        f"🔄 **Cᴏɴᴛᴀᴄᴛ ᴀᴅᴍɪɴ ɪғ ʏᴏᴜ ᴛʜɪɴᴋ ᴛʜɪꜱ ɪꜱ ᴀ ᴍɪꜱᴛᴀᴋᴇ.**\n\n@TenxHubSupport"
                     )
                 except:
                     pass  # mute errors
@@ -31,7 +31,7 @@ async def channel_receive_handler(bot: Client, broadcast: Message):
         file = broadcast.document or broadcast.video
         file_name = file.file_name if file else "Unknown File"
         msg = await broadcast.forward(chat_id=BIN_CHANNEL)
-        raw_stream = f"{URL}watch/{msg.id}/avbotz.mkv?hash={get_hash(msg)}"
+        raw_stream = f"{URL}watch/{msg.id}/xpbotz.mkv?hash={get_hash(msg)}"
         raw_download = f"{URL}{msg.id}?hash={get_hash(msg)}"
         raw_file_link = f"https://t.me/{temp.U_NAME}?start=file_{msg.id}"
         if IS_SHORTLINK:
